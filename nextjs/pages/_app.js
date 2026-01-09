b5ef22f2cd86f83c7ba7502b928a7d8721c6edf9
vireshdeshamane <waynegothambrucebat@gmail.com>
2026-01-09 15:46:15 +0530

    fix: remove server-side database initialization from client-side bundle to fix build error

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,17 +1,4 @@
-'use client';
-import React from 'react';
-import { initializeDatabase } from '../lib/db';
-
 function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    // Initialize database on app startup
-    initializeDatabase().then(result => {
-      console.log('Database initialization result:', result);
-    }).catch(err => {
-      console.error('Database initialization error:', err);
-    });
-  }, []);
-
   return <Component {...pageProps} />
 }
 
